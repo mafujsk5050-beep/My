81e8291ce5a50f5492fe11548f77d0df6a470abe
smartqr <mafujsk5050@gmail.com>
2026-09-10 15:36:30 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>SmartQR Restaurant</h1>
+      <p>Scan Table QR Code to view menu and order food.</p>
+    </div>
+  );
+}
